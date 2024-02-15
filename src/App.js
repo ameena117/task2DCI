5923681588ff8c81676ec96e2e8507f43b2dbfaf
ameena117 <ameenajadallah@gmail.com>
2024-02-15 19:31:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import randomColor from "randomcolor";
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [num, setNum] = useState(0);
+  const [text, setText] = useState("");
+  const [state, setState] = useState(true);
+  useEffect(() => {}, [state]);
+  const [bgcolor, setBGColor] = useState("");
+  const [color, setColor] = useState("");
+  useEffect(() => {}, [color]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ backgroundColor: bgcolor, color: color }}>
+      <button
+        onClick={() => {
+          setNum(num + 1);
+        }}
+      >
+        Increment Number
+      </button>
+      <div>{num}</div>
+
+      <button
+        onClick={() => {
+          setText("Hello");
+        }}
+      >
+        Change Text To Hello
+      </button>
+      <div>{text}</div>
+
+      <button
+        onClick={() => {
+          setState((prev) => !prev);
+        }}
+      >
+        Change State
+      </button>
+
+      <div>{state ? "True" : "False"}</div>
+
+      <input
+        type="text"
+        onChange={(e) => setBGColor(e.target.value)}
+        placeholder="Write a Color"
+      />
+      <hr />
+      <button
+        onClick={() => {
+          setBGColor(randomColor().toString(16));
+        }}
+      >
+        Change BG Color Random
+      </button>
+
+      <button
+        onClick={() => {
+          const color =
+            "#" + Math.round(Math.random() * (256 * 256 * 256)).toString(16);
+          setColor(color);
+        }}
+      >
+        Change Color Random
+      </button>
     </div>
   );
 }
-
-export default App;
